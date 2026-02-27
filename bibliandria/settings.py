616c8f3499fab572f24261508e9b6d0c151ddfde
Alonso Almodóvar <alonsoalmodovar09@gmail.com>
2026-02-27 12:32:44 +0100

    Error login

--- bibliandria/settings.py
+++ bibliandria/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-bibliandria-dev-key-2024-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Permitir todas las direcciones IP (solo para desarrollo)
 
 
 # Application definition
